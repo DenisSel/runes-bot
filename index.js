e7caacfdedaa1e0a0a9357f156dbb89cad807521
Denis Sel <sfaiross@yandex.ru>
2019-03-01 00:55:22 +0700

    Add base structures bot

---
+++ index.js
@@ -0,0 +1,8 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '714012713:AAER2HFO8xw7P8IMauKJdbkswgA6MhhhszM';
+let bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', msg => {
+  bot.sendMessage(msg.chat.id, `Hello from Denis, bot says: "Hi, ${msg.from.first_name}"`)
+});
