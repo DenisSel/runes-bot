9136e5b4dcb28473f443ea1b3ec34ac09bf52b1b
Denis Sel <sfaiross@yandex.ru>
2019-03-02 00:48:47 +0700

    Add code show photo

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const token = '714012713:AAER2HFO8xw7P8IMauKJdbkswgA6MhhhszM';
 let bot = new TelegramBot(token, {polling: true});
 
 bot.on('message', msg => {
-  bot.sendMessage(msg.chat.id, `Hi from Denis, bot says: "Hi, ${msg.from.first_name}"`)
-});
-bot.sendPhoto({
-  chatId: msg.chat.id,
-  caption: 'Test caption',
-  photo: '../src/img/1.jpg'
-}, function(err, msg) {
-  console.log(err);
-  console.log(msg);
+  bot.sendMessage(msg.chat.id, `Hi from Denis, bot says: "Hi, ${msg.from.first_name}"`),
+    bot.sendPhoto({
+      chatId: msg.chat.id,
+      caption: 'Test caption',
+      photo: '../change-db-shop-url.png'
+    }, function(err, msg) {
+      console.log(err);
+      console.log(msg);
+    });
 });
